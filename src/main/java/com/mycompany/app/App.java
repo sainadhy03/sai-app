46154d1a16eb6c18b566d4d50d19467dd33c1819
sainadh <ysainadh03@gmail.com>
2026-04-21 09:28:41 +0000

    pushing my app

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+   This is sainadh's maven project
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
